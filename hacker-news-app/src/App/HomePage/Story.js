dbe110c579556a8324f39c1ebfd1af36643b6577
dunjamarc <dunjamarkovic86@gmail.com>
2018-06-23 12:08:15 +0200

    story

---
+++ hacker-news-app/src/App/HomePage/Story.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Story = (props) => {
+
+    return (
+        <div className="story">
+            <h2>{props.value.title}</h2>
+            <p>by {props.value.by}</p>
+            <p>score {props.value.score}</p>
+            <p>comments</p>
+        </div>
+
+    );
+};
+
+export default Story;
