dba7e959cb73a5c9fabfc62f7fdb5118dbda247c
dunjamarc <dunjamarkovic86@gmail.com>
2018-06-23 12:21:46 +0200

    entities

---
+++ hacker-news-app/src/entities/StoryClass.js
@@ -0,0 +1,15 @@
+class Story {
+    constructor(obj){
+        this.by = obj.by
+        this.descendants = obj.descendants
+        this.id = obj.id
+        this.kids = obj.kids
+        this.score = obj.score
+        this.time = obj.time
+        this.title = obj.title
+        this.type = obj.type
+        this.url = obj.url
+    }
+}
+
+export default Story
