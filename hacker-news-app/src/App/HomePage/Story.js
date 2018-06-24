eaa6eb34ddbe65b684ee53ddf8bea9c36ac4bdea
dunjamarc <dunjamarkovic86@gmail.com>
2018-06-24 16:02:51 +0200

    comment structure

--- hacker-news-app/src/App/HomePage/Story.js
+++ hacker-news-app/src/App/HomePage/Story.js
@@ -1,28 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Story = (props) => {
-    
-    const numComments = () => {
+class Story extends Component {
+
+    /*constructor(props) {
+        super(props);
+        this.state = {
+            topStories: []
+        };
+    }*/
+
+    numComments = () => {
         let count = "";
-        if(props.value.kids === undefined){
+        if (this.props.value.kids === undefined) {
             count = "0 comments";
-        } else if(props.value.kids.length === 1){
+        } else if (this.props.value.kids.length === 1) {
             count = "1 comment";
         } else {
-            count = `${props.value.kids.length} comments`;
+            count = `${this.props.value.kids.length} comments`;
         }
         return count;
-    } 
+    }
 
-    return (
-        <div className="story">
-            <a href={props.value.url} target="_blank">{props.value.title}</a>
-            <p>by {props.value.by}</p>
-            <p>score {props.value.score}</p>
-            <p>{numComments()}</p>
-        </div>
+    render() {
+        return (
+            <div className="story">
+                <a href={this.props.value.url} target="_blank">{this.props.value.title}</a>
+                <p>- by {this.props.value.by}</p>
+                <p>- score {this.props.value.score}</p>
+                <p>- {this.numComments()} -</p>
+                <div className="comments">
+                    
+                </div>
+            </div>
 
-    );
+        );
+    }
 };
 
 export default Story;
